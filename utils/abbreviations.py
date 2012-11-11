9a4fc067a160581cc8a9ad12bba3a791c0683985
James R Douberley <flipdog@mit.edu>
2012-11-10 23:40:47 -0500

    added abbreviation list as synonyms
    there's a couple thousand abbreviations pulled from wikipedia and a few
    other sites, then some entered by hand. we should be able to continue to
    add new ones we find to abbr_text_list.txt to extend it indefinitely.

---
+++ utils/abbreviations.py
@@ -0,0 +1,43 @@
+
+def pull_from_file( file_name):
+    abbreviation_dictionary = {}
+    with open(file_name, "r") as reader:
+        for line in reader.readlines():
+            tokens = line.upper().replace(","," ").split()
+            syn = False
+            definition = []
+            abbs = []
+            for token in tokens:
+                if not syn:
+                    if token == "=":
+                        syn = True
+                    else:
+                        definition.append(token)
+                else:
+                    if token[0] == "(" or token[-1] == ")":
+                        pass
+                    else:
+                        abbs.append(token)
+            if len(definition) == 1:
+                defi = definition[0]
+            elif len(definition) == 0:
+                print 'We have a problem', tokens, line
+            else:
+                defi = definition[0]
+                for j in definition[1:]:
+                    defi += "_" + j
+
+            if not defi in abbreviation_dictionary.keys():
+                if len(abbs) == 0:
+                    print 'We have a problem'
+                    print line
+                else:
+                    abbreviation_dictionary[defi] = [abbs[0]]
+            for abb in abbs:
+                if not abb in abbreviation_dictionary[defi]:
+                    if not abb == defi[0]:
+                        abbreviation_dictionary[defi] = abbreviation_dictionary[defi] + [abb]
+    return abbreviation_dictionary
+
+ABBREVIATIONS = pull_from_file('data/abbr_text_list.txt')
+
--- utils/synonyms.py
+++ utils/synonyms.py
@@ -1,7 +1,7 @@
 import cPickle as pickle
 from collections import defaultdict
 from utils.cryptics import additional_synonyms
-
+from utils.abbreviations import ABBREVIATIONS as abbreviations
 
 def load_synonyms():
     with open('data/synonyms.pck', 'rb') as f:
@@ -12,6 +12,8 @@ def load_synonyms():
 SYNONYMS = load_synonyms()
 for s in additional_synonyms:
     SYNONYMS[s].extend(additional_synonyms[s])
+for s in abbreviations:
+    SYNONYM[s].extend(abbreviations[s])
 
 
 def cached_synonyms(x, length=None):
