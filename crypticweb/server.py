56cd9cbeb5e92633611179a3f3418c383225784b
Robin Deits <robin.deits@gmail.com>
2012-12-18 18:56:43 -0500

    Restructured solver and web interface for threaded solving.
    This also allows us to halt the solver from the web interface without
    crashing everything

--- crypticweb/server.py
+++ crypticweb/server.py
@@ -1,38 +1,52 @@
 import web
 from web import form
-from solve_factored_clue import solve_clue_text, stop_go_server
-
-render = web.template.render('crypticweb/templates/')
-
-urls = ('/', 'index')
-app = web.application(urls, globals())
-
-myform = form.Form(
-    form.Textbox("Clue", size="100"))
+from solve_factored_clue import CrypticClueSolver, split_clue_text
+import threading
 
 
 class index:
     def GET(self):
-        form = myform()
-        return render.index([], form)
+        # form = myform()
+        answers = solver.answers_with_clues
+        if answers == []:
+            answers = ["Sorry, I couldn't find any answers"]
+        elif answers is None:
+            answers = [""]
+        return render.index(answers[:200], form)
 
     def POST(self):
-        form = myform()
+        # form = myform()
         if not form.validates():
             return render.index(["Sorry, something went wrong with that clue"], form)
         else:
             # phrases, answer = parse_clue_text(form.d.Clue)
             # answers = solve_phrases(phrases)[:50]
-            if len(form.d.Clue.split('(')[0].strip().split(' ')) > 7:
-                return render.index("Sorry, I can't reliably handle clues longer than 7 phrases yet. Try grouping some words into phrases by putting an underscore instead of a space between them")
-            answers = solve_clue_text(form.d.Clue)
-            if answers == []:
-                answers = ["Sorry, I couldn't find any answers"]
-            return render.index(answers[:200], form)
+            phrases, lengths, pattern, answer = split_clue_text(form.d.Clue)
+            if len(phrases) > 7:
+                return render.index(["Sorry, I can't reliably handle clues longer than 7 phrases yet. Try grouping some words into phrases by putting an underscore instead of a space between them"], form)
+            solver_thread = threading.Thread(target=solver.solve_clue_text(form.d.Clue))
+            solver_thread.start()
+            solver_thread.join()
+            # answers = solve_clue_text(form.d.Clue)
+            raise web.seeother('/')
+
+
+class halt:
+    def POST(self):
+        print "trying to halt"
+        solver.stop()
+        raise web.seeother('/')
 
 
 if __name__ == "__main__":
-    try:
+    render = web.template.render('crypticweb/templates/')
+
+    urls = ('/', 'index',
+            '/halt', 'halt')
+
+    myform = form.Form(
+        form.Textbox("Clue", size="100"))
+    form = myform()
+    with CrypticClueSolver() as solver:
+        app = web.application(urls, globals())
         app.run()
-    finally:
-        stop_go_server()
--- solve_factored_clue.py
+++ solve_factored_clue.py
@@ -5,15 +5,6 @@ from utils.phrasings import phrasings
 import subprocess
 import re
 
-global go_proc
-go_proc = subprocess.Popen(['cryptics'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-
-def stop_go_server():
-    global go_proc
-    go_proc.stdin.write('..\n')
-    go_proc.wait()
-
 
 class AnnotatedAnswer:
     def __init__(self, ans, clue):
@@ -29,29 +20,82 @@ class AnnotatedAnswer:
         return str([self.answer, self.similarity, self.clue])
 
 
-def solve_clue_text(clue_text):
-    """
-    Solve a raw clue, like
-    Initially babies are naked (4) b... | BARE
-    """
-    clue_text = clue_text.encode('ascii', 'ignore')
-    # solved_parts = dict()
-    all_phrasings, answer = parse_clue_text(clue_text)
-    answers_with_clues = []
+class CrypticClueSolver(object):
+    def __init__(self):
+        self.running = False
+        self.answers_with_clues = None
 
-    go_proc.stdin.write("# %s %s\n" % (all_phrasings[0][-2], all_phrasings[0][-1]))
-    print go_proc.stdout.readline()
-    for p in all_phrasings:
-        print p
-        for ann_ans in solve_phrasing(p, go_proc):
-            answers_with_clues.append(ann_ans)
-        answers_with_clues.sort(reverse=True)
-        if len(answers_with_clues) > 0 and answers_with_clues[0].similarity > 0.85:
-            return answers_with_clues
-    return answers_with_clues
+    def __enter__(self):
+        self.start_go_server()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.stop()
+        self.stop_go_server()
+
+    def start_go_server(self):
+        self.go_proc = subprocess.Popen(['cryptics'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    def stop_go_server(self):
+        self.go_proc.stdin.write('..\n')
+        self.go_proc.wait()
+
+    def stop(self):
+        self.running = False
+
+    def solve_clue_text(self, clue_text):
+        """
+        Solve a raw clue, like
+        Initially babies are naked (4) b... | BARE
+        """
+        self.running = True
+        clue_text = clue_text.encode('ascii', 'ignore')
+        # solved_parts = dict()
+        all_phrasings, answer = parse_clue_text(clue_text)
+        self.answers_with_clues = []
+
+        self.go_proc.stdin.write("# %s %s\n" % (all_phrasings[0][-2], all_phrasings[0][-1]))
+        print self.go_proc.stdout.readline()
+        for p in all_phrasings:
+            if not self.running:
+                self.stop_go_server()
+                self.start_go_server()
+                break
+            print p
+            for ann_ans in self.solve_phrasing(p):
+                self.answers_with_clues.append(ann_ans)
+            self.answers_with_clues.sort(reverse=True)
+            if len(self.answers_with_clues) > 0 and self.answers_with_clues[0].similarity > 0.85:
+                break
+
+    def solve_phrasing(self, phrasing):
+        """
+        Solve a clue which has been broken down into phrases, like:
+        ['initially', 'babies', 'are', 'naked', 4, 'b...']
+        """
+        pattern = phrasing.pop()
+        lengths = phrasing.pop()
+        answers_with_clues = []
+        possible_clues = list(generate_clues(phrasing))
+
+        for i, clue in enumerate(possible_clues):
+            self.go_proc.stdin.write(str(clue) + '\n')
+        self.go_proc.stdin.write('.\n')
+        for i, x in enumerate(possible_clues):
+            result = self.go_proc.stdout.readline()
+            while result.strip() != ".":
+                clue = eval(result)
+                result = self.go_proc.stdout.readline()
+                if clue == []:
+                    continue
+                answer = clue[-1].lower()
+                if answer in phrasing:
+                    continue
+                answers_with_clues.append(AnnotatedAnswer(answer, clue))
+        return sorted(answers_with_clues, reverse=True)
 
 
-def parse_clue_text(clue_text):
+def split_clue_text(clue_text):
     if '|' not in clue_text:
         clue_text += ' |'
     clue_text = clue_text.lower()
@@ -66,48 +110,15 @@ def parse_clue_text(clue_text):
     clue = re.sub(r'\ +', ' ', clue)
     phrases = clue.split(' ')
     phrases = [p for p in phrases if p.strip() != '' and p.strip() != '_']
+    return phrases, lengths, pattern, answer
+
+
+def parse_clue_text(clue_text):
+    phrases, lengths, pattern, answer = split_clue_text(clue_text)
     all_phrasings = []
     for p in phrasings(phrases):
         p += [lengths, pattern]
         all_phrasings.append(p)
     return all_phrasings, answer
 
-
-def solve_phrasing(phrasing, go_proc):
-    """
-    Solve a clue which has been broken down into phrases, like:
-    ['initially', 'babies', 'are', 'naked', 4, 'b...']
-    """
-    pattern = phrasing.pop()
-    lengths = phrasing.pop()
-    answers_with_clues = []
-    possible_clues = list(generate_clues(phrasing))
-
-    for i, clue in enumerate(possible_clues):
-        # print clue
-        go_proc.stdin.write(str(clue) + '\n')
-    go_proc.stdin.write('.\n')
-    for i, x in enumerate(possible_clues):
-        result = go_proc.stdout.readline()
-        while result.strip() != ".":
-            clue = eval(result)
-            result = go_proc.stdout.readline()
-            if clue == []:
-                continue
-            answer = clue[-1].lower()
-            if answer in phrasing:
-                continue
-            answers_with_clues.append(AnnotatedAnswer(answer, clue))
-    return sorted(answers_with_clues, reverse=True)
-
-
-if __name__ == '__main__':
-    # print solve_clue_text(u'spin broken shingle (7)')
-    print solve_clue_text(u'youth climbing with ultimately good cheer (10) .d.......t')
-    print solve_clue_text(u'youth climbing with ultimately good cheer (10) .d.......t')
-    print solve_clue_text(u'youth climbing with ultimately good cheer (10) .d.......t')
-    # for clue in open('clues/clues.txt', 'r').readlines():
-    #     print solve_clue_text(clue)[:1]
-        # break
-    stop_go_server()
 
