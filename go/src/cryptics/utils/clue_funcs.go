78aee29a9bf8952566ac0764c47ba8080e49fdc5
Robin Deits <robin.deits@gmail.com>
2012-12-22 01:44:29 -0500

    Cleared enumeration of possible substrings

--- go/src/cryptics/utils/clue_funcs.go
+++ go/src/cryptics/utils/clue_funcs.go
@@ -45,13 +45,24 @@ func bigram_filter(answers map[string]bool, lengths []int, threshold int) map[st
 	return answers
 }
 
+// First 1-3 letters
+// last letter
+// outside two letters
+// inside 1 or 2 letters
+// all but first
+// all but last
+// all but center
+// all but edges
 func AllLegalSubstrings(words []string, phrasing Phrasing) map[string]bool {
-	if len(words) > 1 {
+	if len(words) != 1 {
 		panic("Word must be [1]string")
 	}
 	length := Sum(phrasing.Lengths)
 	subs := map[string]bool{}
 	word := strings.ToLower(words[0])
+	if len(word) <= 1 {
+		return subs
+	}
 	if strings.Contains(word, "_") {
 		word = strings.Replace(word, "_", "", -1)
 		for i := 0; i < len(word)-length+1; i++ {
@@ -61,30 +72,69 @@ func AllLegalSubstrings(words []string, phrasing Phrasing) map[string]bool {
 			}
 		}
 	} else {
-		for l := 1; l < min(len(word), length+1, 4); l++ {
-			new_subs := legal_substrings(word, l)
-			for s := range new_subs {
-				subs[s] = true
+		for l := 1; l <= min(len(word)-1, length, 3); l++ {
+			subs[word[:l]] = true // first l letters
+		}
+		subs[word[len(word)-1:]] = true // last letter
+		if len(word) > 2 {
+			subs[word[:1]+word[len(word)-1:]] = true // outside
+			if len(word)%2 == 0 {
+				subs[word[len(word)/2-1:len(word)/2+1]] = true         // center
+				subs[word[:len(word)/2-1]+word[len(word)/2+1:]] = true // all but center
+			} else {
+				subs[word[len(word)/2:len(word)/2+1]] = true         // center
+				subs[word[:len(word)/2]+word[len(word)/2+1:]] = true // all but center
 			}
+			subs[word[1:]] = true            // all but first
+			subs[word[:len(word)-1]] = true  // all but last
+			subs[word[1:len(word)-1]] = true // all but edges
 		}
 	}
 	return subs
 }
 
-func legal_substrings(word string, length int) map[string]bool {
-	result := map[string]bool{}
-	result[word[0:length]] = true
-	result[word[len(word)-length:]] = true
-	if (len(word)%2 == 0) && (length == 2) {
-		result[word[len(word)/2-1:len(word)/2+1]] = true
-	}
-	if length == 2 {
-		result[string(word[0])+string(word[len(word)-1])] = true
-	} else if (len(word)%2 == 1) && (length == 1 || length == 3) {
-		result[word[len(word)/2-length/2:len(word)/2+length/2+1]] = true
-	}
-	return result
-}
+// func AllLegalSubstrings(words []string, phrasing Phrasing) map[string]bool {
+// 	if len(words) > 1 {
+// 		panic("Word must be [1]string")
+// 	}
+// 	length := Sum(phrasing.Lengths)
+// 	subs := map[string]bool{}
+// 	word := strings.ToLower(words[0])
+// 	if strings.Contains(word, "_") {
+// 		word = strings.Replace(word, "_", "", -1)
+// 		for i := 0; i < len(word)-length+1; i++ {
+// 			s := word[i : i+length]
+// 			if _, ok := (SYNONYMS)[s]; ok {
+// 				subs[s] = true
+// 			}
+// 		}
+// 	} else {
+// 		for l := 1; l < min(len(word), length+1, 4); l++ {
+// 			new_subs := legal_substrings(word, l)
+// 			for s := range new_subs {
+// 				subs[s] = true
+// 			}
+// 		}
+// 	}
+// 	if len(word) % 2 == 1 {
+// 		subs[
+// 	return subs
+// }
+
+// func legal_substrings(word string, length int) map[string]bool {
+// 	result := map[string]bool{}
+// 	result[word[0:length]] = true
+// 	result[word[len(word)-length:]] = true
+// 	if (len(word)%2 == 0) && (length == 2) {
+// 		result[word[len(word)/2-1:len(word)/2+1]] = true
+// 	}
+// 	if length == 2 {
+// 		result[string(word[0])+string(word[len(word)-1])] = true
+// 	} else if (len(word)%2 == 1) && (length == 1 || length == 3) {
+// 		result[word[len(word)/2-length/2:len(word)/2+length/2+1]] = true
+// 	}
+// 	return result
+// }
 
 func min(x ...int) int {
 	result := x[0]
