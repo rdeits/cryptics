1adc676d7b8e60dd5cb4ba5579bcdca461c45a4b
Robin Deits <robin.deits@gmail.com>
2012-12-25 04:47:18 -0500

    preallocate

--- go/src/cryptics/main.go
+++ go/src/cryptics/main.go
@@ -12,17 +12,11 @@ import (
 )
 
 func main() {
-	runtime.GOMAXPROCS(8)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	var phrasing utils.Phrasing
-	var pattern string
-	var lengths []int
-	var l int
 	var num_clues int
+	var l int
 	var clue string
-	var solved_clue solver.StructuredClue
-	var parts []string
-	var lengths_str string
-	var lengths_strs []string
 	solved_parts := map[string]map[string][]string{}
 	stdin := bufio.NewReader(os.Stdin)
 	ans_c := make(chan solver.StructuredClue)
@@ -37,7 +31,7 @@ func main() {
 			break
 		} else if clue == "." {
 			for i := 0; i < num_clues; i++ {
-				solved_clue = <-ans_c
+				solved_clue := <-ans_c
 				for _, a := range solved_clue.FormatAnswers() {
 					fmt.Println(a)
 				}
@@ -46,11 +40,11 @@ func main() {
 			num_clues = 0
 		} else if string(clue[0]) == "#" {
 			num_clues = 0
-			parts = strings.Split(clue, "(")
+			parts := strings.Split(clue, "(")
 			parts = strings.Split(parts[1], ")")
-			lengths_str = parts[0]
-			lengths = []int{}
-			lengths_strs = strings.Split(lengths_str, ",")
+			lengths_str := parts[0]
+			lengths := []int{}
+			lengths_strs := strings.Split(lengths_str, ",")
 			for _, c := range lengths_strs {
 				if strings.TrimSpace(c) == "" {
 					continue
@@ -58,7 +52,7 @@ func main() {
 				fmt.Sscanf(c, "%d", &l)
 				lengths = append(lengths, int(l))
 			}
-			pattern = strings.TrimSpace(parts[1])
+			pattern := strings.TrimSpace(parts[1])
 			solved_parts = map[string]map[string][]string{}
 			phrasing = utils.Phrasing{Lengths: lengths, Pattern: pattern}
 			fmt.Println(phrasing)
