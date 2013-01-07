d05216d6d306add6d65ac087a96d23a12c2a036b
Robin Deits <robin.deits@gmail.com>
2013-01-07 02:00:39 -0500

    Add serial mode for debugging

--- python/solve_clue.py
+++ python/solve_clue.py
@@ -103,20 +103,42 @@ class CrypticClueSolver(object):
         answers_with_clues = []
         possible_clues = list(generate_clues(phrasing))
 
-        for i, clue in enumerate(possible_clues):
-            self.go_proc.stdin.write(str(clue) + '\n')
-        self.go_proc.stdin.write('.\n')
-        for i, x in enumerate(possible_clues):
-            result = self.go_proc.stdout.readline()
-            while result.strip() != ".":
-                clue = eval(result)
+        parallel = True
+
+        if parallel:
+            for i, clue in enumerate(possible_clues):
+                self.go_proc.stdin.write(str(clue) + '\n')
+            self.go_proc.stdin.write('.\n')
+            for i, x in enumerate(possible_clues):
                 result = self.go_proc.stdout.readline()
-                if clue == []:
-                    continue
-                answer = clue[-1].lower()
-                if answer in phrasing or any(x.startswith(answer) for x in phrasing):
-                    continue
-                answers_with_clues.append(AnnotatedAnswer(answer, clue))
+                while result.strip() != ".":
+                    clue = eval(result)
+                    result = self.go_proc.stdout.readline()
+                    if clue == []:
+                        continue
+                    answer = clue[-1].lower()
+                    if answer in phrasing or any(x.startswith(answer) for x in phrasing):
+                        continue
+                    answers_with_clues.append(AnnotatedAnswer(answer, clue))
+        else:
+            for i, clue in enumerate(possible_clues):
+                # print clue
+                self.go_proc.stdin.write(str(clue) + '\n')
+                self.go_proc.stdin.write('.\n')
+                result = self.go_proc.stdout.readline()
+                while result.strip() != ".":
+                    if result[0] == "#":
+                        print result
+                        result = self.go_proc.stdout.readline()
+                        continue
+                    clue = eval(result)
+                    result = self.go_proc.stdout.readline()
+                    if clue == []:
+                        continue
+                    answer = clue[-1].lower()
+                    if answer in phrasing or any(x.startswith(answer) for x in phrasing):
+                        continue
+                    answers_with_clues.append(AnnotatedAnswer(answer, clue))
         return sorted(answers_with_clues, reverse=True)
 
     def collect_answers(self):
